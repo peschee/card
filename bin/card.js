c795351af42e13a4a8c22971ec2d116567481cf2
Tierney Cyren <hello@bnb.im>
2018-10-31 17:22:32 -0400

    Changes for peschee

--- bin/card.js
+++ bin/card.js
@@ -1,24 +1,54 @@
 #!/usr/bin/env node
 
-var chalk = require("chalk")
+'use strict'
 
-var twitter = chalk.cyan("@bitandbang")
-var github = chalk.cyan("@bnb")
-var linkedin = chalk.cyan("@bitandbang")
-var site = chalk.cyan("https://bnb.im")
-var npx = "npx bitandbang"
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
 
-console.log("")
-console.log(chalk.green("╔══════════════════════════════════════════════════════════════════════╗"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("║  ") + "Tierney Cyren – " + chalk.cyan("bitandbang") + chalk.green("                                          ║"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("║  ") + chalk.bold("Twitter:   ") + twitter + chalk.green("                                              ║"))
-console.log(chalk.green("║  ") + chalk.bold("GitHub:    ") + github + chalk.green("                                                     ║"))
-console.log(chalk.green("║  ") + chalk.bold("LinkedIn:  ") + linkedin + chalk.green("                                              ║"))
-console.log(chalk.green("║  ") + chalk.bold("Web:       ") + site + chalk.green("                                           ║"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("║  ") + chalk.bold("Card: ") + npx + chalk.green("                                                ║"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("╚══════════════════════════════════════════════════════════════════════╝"))
-console.log("")
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('              Peter Siska'),
+  handle: chalk.white('peschee'),
+  work: chalk.white('Executive Board Member at Inventage AG'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('peschee'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~peschee'),
+  github: chalk.gray('https://github.com/') + chalk.green('peschee'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('pesche'),
+  npx: chalk.red('npx') + ' ' + chalk.white('peschee'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelTwitter: chalk.white.bold('    Twitter:'),
+  labelnpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelCard: chalk.white.bold('       Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} / ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const npming = `${data.labelnpm}  ${data.npm}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               twittering + newline + // data.labelTwitter + data.twitter
+               npming + newline + // data.labelnpm + data.npm
+               githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               carding // data.labelCard + data.npx
+
+console.log(chalk.green(boxen(output, options)))
