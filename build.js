40663ae3ef3b10b7b5e077f7842c1fac2ad0a5a8
Peter Siska <psiska@inventage.com>
2018-12-27 10:52:39 +0100

    Add work url, add spacing between sections.

--- build.js
+++ build.js
@@ -18,6 +18,7 @@ const data = {
   name: 'Peter Siska',
   handle: 'peschee',
   work: 'Executive Board Member at Inventage AG',
+  workUrl: 'https://www.inventage.com',
   twitter: 'https://twitter.com/peschee',
   npm: 'https://npmjs.com/~peschee',
   github: 'https://github.com/peschee',
@@ -33,8 +34,9 @@ const data = {
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = chalk.white(`${chalk.bold(data.name)} / ${data.handle}`)
-const working = chalk.white(`${chalk.bold(data.labelWork)}      ${data.work}`)
+const heading = chalk.white(`           ${chalk.bold(data.name)} / ${data.handle}`)
+let working = chalk.white(`${chalk.bold(data.labelWork)}      ${data.work}`)
+working += chalk.white(`${newline}           ${data.workUrl}`)
 const twittering = chalk.white(`${chalk.bold(data.labelTwitter)}   ${data.twitter}`)
 const npming = chalk.white(`${chalk.bold(data.labelnpm)}       ${data.npm}`)
 const githubing = chalk.white(`${chalk.bold(data.labelGitHub)}    ${data.github}`)
@@ -44,11 +46,11 @@ const carding = chalk.white(`${chalk.bold(data.labelCard)}      ${data.npx}`)
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
+               working + newline + newline + // data.labelWork + data.work + data.workUrl
                twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-               carding // data.labelCard + data.npx
+               npming + newline + // data.labelnpm + data.npm
+               newline + carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
