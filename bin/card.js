128216b8a0b0b3a4afc4b5f5dd1cc135b28100b0
Peter Siska <63866+peschee@users.noreply.github.com>
2022-09-20 13:21:34 +0200

    Fix imports

--- bin/card.js
+++ bin/card.js
@@ -2,8 +2,12 @@
 
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
 
 console.log(output)
